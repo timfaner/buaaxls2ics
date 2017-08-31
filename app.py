46370e1bf8a98d68a1dce4f38919556cb22bd276
timfaner <grngrngrngrn@163.com>
2017-09-01 00:17:11 +0800

    improve  behavioor

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-import os, sqlite3,hashlib,qrcode
-from flask import Flask, request, redirect, url_for, abort, send_from_directory,g,session,render_template
+import os, sqlite3,hashlib,json
+from flask import Flask, request, redirect, url_for, abort, send_from_directory,g,session,render_template,make_response
 from werkzeug import secure_filename
 import xls2ics,config
 from tools import randstr
-"""
-import logging  
-import logging.config  
-import xls2ics
+import qrcode
 
-logging.config.fileConfig('logging_config.ini')
-logger = logging.getLogger('root')
-"""
 
-ALLOWED_EXTENSIONS = set(['xls','txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+if not app.debug:
+    import logging  
+    import logging.config  
+    logging.config.fileConfig('logging_config.ini')
+    logger = logging.getLogger('root')
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -24,91 +24,129 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template('cover.html')
+    
+    if 'qr_path' in session:
+        return render_template('qr.html',qr_path = session['qr_path'])
+    else:
+        return render_template('index.html')
+
+
+
+@app.route('/process',methods=['GET','POST'])
+def process():
     if request.method == 'POST':
         file = request.files['file']
         
-        if file and allowed_file(file.filename):
-            
-            
+        if file :
             xls_dict = {
                     'campus':request.form['area'],\
-                    'term_season':request.form['term_season'],\
-                    'term_year':request.form['term_year']}
+                    'term_season':'fail',\
+                    'term_year':'2017'}
 
             file_content = file.read()
             md5 = calMd5(file_content,xls_dict)
-            print(md5)
+           
             
             cursor = g.db.cursor()
             cursor.execute('select md5 from files_info where md5=?',(md5,))
             md5_exists = cursor.fetchall()
+
             url = ''
-            
+            errorcode = 0
+            title = ''
             if not md5_exists:
+                try:
+                    cal = handleXls(file_content,xls_dict,md5)
+                    title = cal.getTitle()
+                    url = randstr(10, url_safe=True)
+                    
+                    with open(os.path.join(app.config['UPLOAD_FOLDER'], md5),'w',encoding='utf-8') as f:
+                        try:
+                            f.writelines(cal.getIcs())
+                        except Exception as e:
+                            app.logger.error("ics saved failed")
+                            raise e
+                    cursor.execute('insert into files_info (md5,title,url) values (?,?,?)',(md5,title,url))
+                    g.db.commit()
+                except Exception as e:
+                    app.logger.error(e)
+                    errorcode = 1
                 
-                cal = handleXls(file_content,xls_dict)
-                title = cal.getTitle()
-                url = randstr(10, url_safe=True)
-                with open(os.path.join(app.config['UPLOAD_FOLDER'], md5),'w',encoding='utf-8') as f:
-                    f.writelines(cal.getIcs())
-                cursor.execute('insert into files_info (md5,title,url) values (?,?,?)',(md5,title,url))
-                g.db.commit()
             else:
-                cursor.execute('select url from files_info where md5=?',(md5,))
-                url = cursor.fetchall()[0][0]
+                app.logger.info('file {} search in db'.format(md5))
+                cursor.execute('select url,title from files_info where md5=?',(md5,))
+                content = cursor.fetchall()
+                url = content[0][0]
+                title = content[0][1]
             
-            qr = qrcode.make(url_for('gtics',uuid=url, _external=True,))
+            qr = qrcode.make(url_for('scan_result',uuid=url, _external=True,))
             qr_id = secure_filename(randstr(13,url_safe=True) + '.png')
+
+            ics_path = url_for('getit',uuid=url, _external=True,)
             qr_path = os.path.join(app.config['QR_FOLDER'],qr_id)
             session['qr_path'] = qr_path
-            qr.save(qr_path)
-            
-            return redirect('/',code=302)
+            try:
+                qr.save(qr_path)
+            except Exception as e:
+                app.logger.error("Qr img saved failed")
+                raise e
+            res = dict(errorcode= errorcode, qr_path= qr_path,ics_path=ics_path, title=title)
+            response=make_response(json.dumps(res))
+            response.headers['Content-Type']='application/json'
+            return response
         else:
-            return '请重新选择文件格式'
-    elif request.method == 'GET':
-        if 'qr_path' in session:
-            return render_template('qr.html',qr_path = session['qr_path'])
-        else:
-            return render_template('index.html')
-    return 'hello'
-    
+            abort(403)
+    else:
+        abort(404)
+
+
+
 @app.route('/reupload')
 def reupload():
     if 'qr_parh' in session:
         session.pop('qr_path',None)
     return redirect(url_for('index'))
 
-@app.route('/ics')
-def gtics(**kw):
-        url = request.args.get('uuid')
-        print(url)
-        cursor = g.db.cursor()
-        cursor.execute('select md5 from files_info where url=?',(url,))
-        md5 = cursor.fetchall()[0][0]
-        print(md5)
-        return send_from_directory(app.config['UPLOAD_FOLDER'], md5)
-    
+@app.route('/getit')
+def getit(**kw):
+    url = request.args.get('uuid')
+    print(url)
+    cursor = g.db.cursor()
+    cursor.execute('select md5,title from files_info where url=?',(url,))
+    content = cursor.fetchall()
+    if not content:
+        abort(404)
+    md5 = content[0][0]
+    title = content[0][1] + '.ics'
+    print(md5)
+    response = make_response(send_from_directory(\
+    app.config['UPLOAD_FOLDER'], md5,  \
+    as_attachment=True, \
+    attachment_filename=title))
+    response.headers["Content-Disposition"] = "attachment; filename={}".format(title.encode().decode('latin-1'))
+    return response
 
+@app.route('/ics')
+def scan_result(**kw):
+    url = request.args.get('uuid')
+    return render_template('scan.html',url=url)
+
+@app.route('/static/<id>')
+def static_server(id):
+    return send_from_directory(app.config['STATIC_FOLDER'],id)
 
 @app.route('/img/<id>')
 def showqr(id):
     id = secure_filename(id)
     return send_from_directory(app.config['QR_FOLDER'],id)
 
-@app.route('/finish_upload<filename>')
-def finish_upload(filename):
-	return ('finish_upload '+ filename)
-
-
-
-
-
 
 @app.before_request
 def before_request():
     get_db()
- 
+        
+
 @app.teardown_request
 def teardown_request(exception):
     db = getattr(g, 'db', None)
@@ -128,13 +166,13 @@ def calMd5(file,xls_dict):
     md5.update(user_str.encode('utf-8'))
     return md5.hexdigest()
 
-def handleXls(file, xls_dict):
+def handleXls(file, xls_dict,md5):
     '''输入文件二进制
         返回ics对象
     '''
     
     
-    cal = xls2ics.XlsParser(xls_content=file)
+    cal = xls2ics.XlsParser(xls_content=file,uid=md5)
     cal.campus = xls_dict['campus']
     
     '''#TODO
@@ -167,4 +205,5 @@ def init_db(): #初始化数据库，人工执行
         db.commit()
 
 if __name__ == '__main__':
-	app.run()
+    app.run('0.0.0.0')
+    
